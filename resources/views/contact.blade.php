440a59858e644a018f855e99fbf623c61b5b32e2
Harley <harleyradovic@gmail.com>
2019-01-10 22:20:07 +1000

    Flexbox cards, new views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts')
+
+@section('title', 'Contact')
+
+@section('navBar')
+    @parent
+@endsection
+
+@section('content')
+    <p> Welcome to my Laravel project </p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,20 @@
 |
 */
 
+Route::get('/welcome', function() {
+    return view('landingPage');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
 
 Auth::routes();
