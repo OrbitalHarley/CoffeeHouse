469abc13bc53e9aecaae33f1042bc9f78ee622c5
Harley <harleyradovic@gmail.com>
2019-01-31 21:51:12 +1000

    Added npm made header and footer into partials

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,42 +1,11 @@
+@extends('layouts')
+
 @section('title', 'Welcome')
-        <!doctype html>
-        <title>Example</title>
-        <style>
-          * {
-           box-sizing: border-box;
-          }
-          body {
-            margin: 0;
-          }
-          #main {
-            display: flex;
-            min-height: calc(100vh - 40vh);
-          }
-          #main > article {
-            flex: 1;
-          }
-          #main > nav,
-          #main > aside {
-            flex: 0 0 20vw;
-            background: beige;
-          }
-          #main > nav {
-            order: -1;
-          }
-          header, footer, article, nav, aside {
-            padding: 1em;
-          }
-          header, footer {
-            background: yellowgreen;
-            height: 20vh;
-          }
-        </style>
-        <body>
-          <header>Header</header>
-          <div id="main">
-            <article>Article</article>
-            <nav>Nav</nav>
-            <aside>Aside</aside>
-          </div>
-          <footer>Footer</footer>
-        </body>
+
+@section('navBar')
+    @parent
+@endsection
+
+@section('content')
+    <p> Welcome to my Laravel project </p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<head>
+    <Title>Coffee House - @yield('title')</Title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</head>
